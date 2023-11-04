4f4136e942d83a321e586a8b1a906a6a705fe497
Afromullet <seankharris91@gmail.com>
2023-11-04 07:00:18 -0400

    Added sine noise generation
    Added sine noise generation

--- Racket/WorldMap.rkt
+++ Racket/WorldMap.rkt
@@ -1,6 +1,7 @@
 #lang racket/gui
 (require racket/include)
 (require  noise)
+(require plot)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;                                Definitions                                                       ;
@@ -10,12 +11,20 @@
 (define MAP-HEIGHT 50)
 (define TILE-SIZE 8)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;                                Perlin Noise Related                                              ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 (define (get-seed)
   (current-inexact-milliseconds))
+
+
+
+
+(define (normalize-sample value min max)
+  (/ (- value min) (- max min)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                                Noise Composition                                              ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
 
 
 ;We use Noise composition. The final noise source equals the sum of:
@@ -26,54 +35,97 @@
 ;Then we normalize the noise at the end using the sum of the amplitudes
 ;Represents the data we need for noise composition.
 ;Stores a list of amplitudes, octaves, and the total amplitude
-(struct noise-params (amplitudes octaves total-amplitude ))
+(struct noise-composition-params (amplitudes octaves total-amplitude ))
 
-;Used to create the noise-params type struct
-(define (create-noise-params amplitudes octaves)
-  (noise-params amplitudes octaves (apply + amplitudes))) 
+;Used to create the noise-composition-params type struct
+(define (create-noise-composition-params amplitudes octaves)
+  (noise-composition-params amplitudes octaves (apply + amplitudes))) 
 
-;Creates the noise-params, given a number of octaves
-(define (build-noise-params num-octaves)
+;Creates the noise-composition-params, given a number of octaves
+(define (build-noise-composition-params num-octaves)
   (let ([amps '()]
         [octs '()])
-  (for/list ([i (in-range 1 (+ num-octaves 1))])
-    (let* ([octave (expt 2 (- i 1))]
-           [amplitude (/ 1.0 octave)])
-      (set! amps (append amps (list amplitude)))
-      (set! octs (append octs (list octave)))))
-         (create-noise-params amps octs)))
+    (for/list ([i (in-range 1 (+ num-octaves 1))])
+      (let* ([octave (expt 2 (- i 1))]
+             [amplitude (/ 1.0 octave)])
+        (set! amps (append amps (list amplitude)))
+        (set! octs (append octs (list octave)))))
+    (create-noise-composition-params amps octs)))
 
-;Gets a single noise sample
-;Divides by the map width and height so we get values between 0-1
-(define (generate-noise amplitude octave x-cord y-cord)
-  (* amplitude (abs(perlin (* octave (/ x-cord MAP-WIDTH)) (* octave (/ y-cord MAP-HEIGHT))))))
 
-; Creates a tile with a simplex noise value
-(define (create-noise-tile x-cord y-cord)
-  (hash 'x x-cord
-        'y y-cord
-        'tile-width (* x-cord TILE-SIZE)
-        'tile-height (* y-cord TILE-SIZE)
-        ;'noise (abs(simplex x-cord y-cord))))
-        'noise (compose-noise noise-source x-cord y-cord)))
 
 ;Composes the noise by adding several noise sources together using octaves and amplitudes.
 ;Adds an offset through a seed so we don't get the same valeu every time. 
 (define (compose-noise source x-cord y-cord)
+  ;Gets a single noise sample
+  ;Divides by the map width and height so we get values between 0-1
+  (define (generate-octave-noise amplitude octave x-cord y-cord)
+    (* amplitude (abs(perlin (* octave (/ x-cord MAP-WIDTH))
+                             (* octave (/ y-cord MAP-HEIGHT))))))
+
   (define noise-values
-    (for/list ([amp (noise-params-amplitudes source)]
-               [oct (noise-params-octaves source)])
-      (generate-noise amp oct (+ x-cord (get-seed)) (+ y-cord (get-seed)))))
-       (normalize-noise (foldl + 0 noise-values) (noise-params-total-amplitude source)))
+    (for/list ([amp (noise-composition-params-amplitudes source)]
+               [oct (noise-composition-params-octaves source)])
+      (generate-octave-noise amp oct (+ x-cord (get-seed)) (+ y-cord (get-seed)))))
+  (normalize-composition-noise (foldl + 0 noise-values) (noise-composition-params-total-amplitude source)))
+
+
+
 
 ;normalizes the noise so that we have values between 0 and 1
-(define (normalize-noise noise-value total-amplitude)
+(define (normalize-composition-noise noise-value total-amplitude)
   (/ noise-value total-amplitude))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;                                Drawing Related                                                   ;
+;                                Trigonometric Functions for Noise                                ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(define (linspace start stop num)
+  (for/list ([i (in-range num)])
+    (+ start (* (/ i (- num 1.0)) (- stop start)))))
+
+;Creates a sine modulated signal
+(define (sine-modulation start-deg end-deg min max sample-num)
+  (let ([radians (* (- end-deg start-deg) (/ pi 180))]
+        [dev (/ (- max min) 2)]
+        [base (/ (+ max min)2)])
+    ;(normalize-sample (+ (* dev (sin (* radians sample-num))) base) min max)
+     (+ (* dev (sin (* radians sample-num))) base) min max)
+    )
+
+
+  (define width-sample-space (linspace 0  100 MAP-WIDTH))
+  (define height-sample-space (linspace 0 100  MAP-HEIGHT))
+
+
+
+
+
+;Gets an x and y sample from a sinusoid and then composes them
+(define (sine-modulated-noise start-deg end-deg min max x-cord y-cord)
+
+  ( let ([x-point (sine-modulation start-deg end-deg min max (list-ref width-sample-space x-cord))]
+        [y-point (sine-modulation start-deg end-deg min max y-cord)])
+     (abs (perlin (+ x-point (get-seed) (+ y-point (get-seed)))))
+
+     ))
+
+
+
+
+(define samples (in-range 0 100))
+
+(define data (for/list ([i samples]) (sine-modulation 0 360 1 1000 i)))
+(define data2 (for/list ([i (in-range MAP-WIDTH)]) (sine-modulated-noise 0 360 100 200 i i)))
+                                                                        
+
+;(plot (points (map vector samples data) #:color 'red))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                                Drawing and Tile Related                                                   ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
 
 ;Need to change this so we don't need to search teh map twice with get-drawing data and get-tile-noise-data
@@ -82,8 +134,8 @@
   (for ([x (in-range MAP-WIDTH)])
     (for ([y (in-range MAP-HEIGHT)])
       (let ([tile (get-tile world-map x y)])
-         (draw-tile-png dc (hash-ref tile 'tile-width)
-                      (hash-ref tile 'tile-height ) (get-noise-png(hash-ref tile 'noise)))))))   
+        (draw-tile-png dc (hash-ref tile 'tile-width)
+                       (hash-ref tile 'tile-height ) (get-noise-png(hash-ref tile 'noise)))))))   
 
 
 (define (draw-tile-png dc x y image-path)
@@ -92,14 +144,14 @@
 
 ;Returns the png for a noise value
 (define (get-noise-png noise-val)
-(cond
-[(< noise-val 0.1) "water1.png"]
-[(< noise-val 0.2) "beachsand1.png"]
-[(< noise-val 0.3) "tree1.png"]
-[(< noise-val 0.5) "jungle1.png"]
-[(< noise-val 0.7) "shrubland1.png"]
-[(< noise-val 0.9) "deserttile1.png"]
-[else  "snowground1.png"]))
+  (cond
+    [(< noise-val 0.1) "water1.png"]
+    [(< noise-val 0.2) "beachsand1.png"]
+    [(< noise-val 0.3) "tree1.png"]
+    [(< noise-val 0.5) "jungle1.png"]
+    [(< noise-val 0.7) "shrubland1.png"]
+    [(< noise-val 0.9) "deserttile1.png"]
+    [else  "snowground1.png"]))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -110,22 +162,46 @@
 (define (get-tile grid x y)
   (vector-ref (vector-ref grid x) y))
 
-
+; Creates a tile with a noise value. The noise-func is any kind of noise generation function
+; That has at least an x-cord and a y-cord. All of the arguments before x-cord and y-cord are curried
+(define (create-noise-tile x-cord y-cord noise-func)
+  (hash 'x x-cord
+        'y y-cord
+        'tile-width (* x-cord TILE-SIZE)
+        'tile-height (* y-cord TILE-SIZE)
+        ;'noise (abs(simplex x-cord y-cord))))
+        'noise (noise-func x-cord y-cord)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;                                World Map Related                                                ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(define (prepare-noise-func noise-func)
+  (lambda (x-cord y-cord)
+    (noise-func noise-source x-cord y-cord)))
+
+(define (prepare-noise-func2 noise-func)
+  (lambda (x-cord y-cord)
+    (noise-func 0 360 1 10000 x-cord y-cord)))
+
+(define (prepare-noise-func3 noise-func optional-args )
+    (lambda (x-cord y-cord)
+    (noise-func (optional-args) x-cord y-cord)))
+
+
+(prepare-noise-func3 sine-modulated-noise '(0 360 1 1000))
 
 (define (initialize-world-map)
   (define world-map (make-vector MAP-WIDTH))
   (for ([x-cord (in-range MAP-WIDTH)])
     (define row (make-vector MAP-HEIGHT))
     (for ([y-cord (in-range MAP-HEIGHT)])
-      (vector-set! row y-cord (create-noise-tile x-cord y-cord)))
+      (vector-set! row y-cord (create-noise-tile x-cord y-cord (prepare-noise-func2 sine-modulated-noise))))
     (vector-set! world-map x-cord row))
   world-map)
 
 
-(define noise-source (build-noise-params 3))
+(define noise-source (build-noise-composition-params 3))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;                                Utility Todo move                                                 ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
