371ece34396def5e99aacf6f44ecf3d128214797
Afromullet <seankharris91@gmail.com>
2023-11-08 10:56:41 -0500

    Added more gui components

--- Racket/DrawMap.rkt
+++ Racket/DrawMap.rkt
@@ -6,56 +6,186 @@
 (require "WorldMap.rkt")
 
 
+;Parameters that modify world map generation
+(struct map-parameters ([distance-func #:mutable]
+                        [noise-func #:mutable]
+                        [redist-fudge #:mutable]
+                        [redist-exp #:mutable]))
 
-(define (world-map) (initialize-world-map))
+(define (create-map-parameters current-dist-func current-noise-func redist-fudge redist-exp)
+  (map-parameters current-dist-func current-noise-func redist-fudge redist-exp))
 
-(define (generate-new-world-map)
-  (set! world-map (initialize-world-map)))
+(define map-params (create-map-parameters no-distance-function noise-composition 1 1))
 
 
 
+(define (world-map) (initialize-world-map (map-parameters-distance-func map-params)
+                                          (map-parameters-noise-func map-params)
+                                          (map-parameters-redist-fudge map-params)
+                                          (map-parameters-redist-exp map-params)))
+
+
+(map-parameters-distance-func map-params)
+
+(define update-world-map
+  (lambda ()
+    (set! world-map (initialize-world-map (map-parameters-distance-func map-params)
+                                          (map-parameters-noise-func map-params)
+                                          (map-parameters-redist-fudge map-params)
+                                          (map-parameters-redist-exp map-params)))world-map))
+
+
+#|_______________________________Distance Function Selection  Helpers_________________________________
+
+Functions that allow the user to select which distance function to apply to the world map
+____________________________________________________________________________________________________|#
+
+; Define a list of noise functions
+(define distance-func-names
+  '("square-bump" "euclidian-squared" "no-distance-function"))
+
+
+(define (set-distance-function choice event canvas)
+  
+  (define distance-functions
+    (hash "square-bump" square-bump
+          "euclidian-squared" euclidian-squared
+          "no-distance-function" no-distance-function))
+  
+  (set-map-parameters-distance-func! map-params (hash-ref distance-functions (send choice get-string-selection))))
+
+
+
+
+#|_______________________________Modulation Function Selection  Helpers_________________________________
+
+Functions that allow the user to select which modulation function to apply to the world map
+____________________________________________________________________________________________________|#
+
+
+; Define a list of noise functions
+(define modulation-func-names
+  '("composition" "sinusoidal" "linear"))
+
+(define (set-modulation-function choice event canvas)
+  (define modulation-functions
+    (hash "composition" noise-composition
+          "sinusoidal" sine-noise
+          "linear" linear-stuff))
+  (set-map-parameters-noise-func! map-params (hash-ref modulation-functions (send choice get-string-selection))))
+
+
+
+#|______________________________Sliders_________________________________
+
+Handles all slider action
+____________________________________________________________________________________________________|#
+
+
+
+(define (get-fudge-factor entry event)
+  (define factor (send entry get-value))
+  (set-map-parameters-redist-fudge! map-params (send entry get-value)))
+
+
+(define (get-expt entry event)
+  (define factor (send entry get-value))
+  (set-map-parameters-redist-exp! map-params (send entry get-value)))
+
+
+
+
+#|_______________________________GUI Compoents_____________________________________________________
+
+Creates the GUI components
+____________________________________________________________________________________________________|#
 
 (define frame (new frame%
-                  [label "Tile Map Example"]
-                  [width (* MAP-WIDTH TILE-SIZE)]
-                  [height (* MAP-HEIGHT TILE-SIZE)]
-                  [alignment '(center center)]))
+                   [label "Tile Map Example"]
+                   [width (* MAP-WIDTH TILE-SIZE)]
+                   [height (* MAP-HEIGHT TILE-SIZE)]
+                   [alignment '(center center)]))
 
 
 
 ; Make a static text message in the frame
 (define msg (new message% [parent frame]
-                          [label "No events so far..."]))
+                 [label "No events so far..."]))
     
-  ; Derive a new canvas (a drawing window) class to handle events
+; Derive a new canvas (a drawing window) class to handle events
 (define my-canvas%
   (class canvas% ; The base class is canvas%
     ; Define overriding method to handle mouse events
     (define/override (on-event event)
       (send msg set-label "Canvas mouse")
       (send msg set-label "Canvas mouse1"))
+    
     ; Define overriding method to handle keyboard events
     (define/override (on-char event)
-
       (world-map)
-       (send canvas refresh)    ; Refresh the canvas to display the new map
-      
-
-
+      (send canvas refresh)    ; Refresh the canvas to display the new map
       )
     ; Call the superclass init, passing on all init args
     (super-new)))
       
 (define canvas (new  my-canvas%
-                   [parent frame]
-                   [min-width (* MAP-WIDTH TILE-SIZE)]
-                   [min-height (* MAP-HEIGHT TILE-SIZE)]
-                   [stretchable-width #f]
-                   [stretchable-height #f]
-                   [paint-callback (lambda (canvas dc)
-                                     (draw-tile-map canvas (world-map)))]))
+                     [parent frame]
+                     [min-width (* MAP-WIDTH TILE-SIZE)]
+                     [min-height (* MAP-HEIGHT TILE-SIZE)]
+                     [stretchable-width #f]
+                     [stretchable-height #f]
+                     [paint-callback (lambda (canvas dc)
+                                       (draw-tile-map canvas (world-map)))]))
 
 
+
+
+; Create a list-box with the noise function options
+(define distance-function-list-box
+  (new list-box%
+       [label "Select Distance Function"] ; Set the label here
+       [choices distance-func-names]
+       [parent frame]
+       [callback
+        (lambda (choice event)
+          (set-distance-function choice event canvas))]))
+
+
+; Create a list-box with the noise function options
+(define modulation-function-list-box
+  (new list-box%
+       [label "Select Modulation Function"] ; Set the label here
+       [choices modulation-func-names]
+       [parent frame]
+       [callback
+        (lambda (choice event)
+          (set-modulation-function choice event canvas))]))
+
+(define generate-new-map-button
+  (new button%
+       [label "Generate new map"]
+       [parent frame]
+       [callback
+        (lambda (choice event )
+          update-world-map(send canvas refresh))]))
+
+
+(define redistribution-fudge-factor
+  (new slider%
+       [label "Redistribution Fudge Factor"]
+       [min-value 0]
+       [max-value 10]
+       [parent frame]
+       [callback get-fudge-factor] ))
+
+
+(define redistribution-exponent
+  (new slider%
+       [label "Redistribution Exponent"]
+       [min-value 0]
+       [max-value 10]
+       [parent frame]
+       [callback get-expt] ))
 
 
 (send frame show #t)
--- Racket/WorldMap.rkt
+++ Racket/WorldMap.rkt
@@ -1,16 +1,16 @@
 #lang racket/gui
 (require racket/include)
+(require racket/file)
 (require  noise)
 (require plot)
 
 
 
+#|________________________________Definitions_______________________________________________
 
+Global defs that will be used throughout the file
+____________________________________________________________________________________________________|#
 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;                                Definitions                                                       ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define MAP-WIDTH 50)
 (define MAP-HEIGHT 50)
@@ -19,19 +19,17 @@
 (define (get-seed)
   (current-inexact-milliseconds))
 
-(define (normalize-sample value min max)
-  (/ (- value min) (- max min)))
 
-#|
-________________________________"Biomes_______________________________________________
+#|________________________________"Biomes_______________________________________________
 
-____________________________________________________________________________________________________
-|#
+Handles biome selection
+____________________________________________________________________________________________________|#
+
 
 
 
 ;Returns the png for a noise value
-(define (get-noise-png noise-val)
+(define (get-biome-data noise-val)
   (cond
     [(< noise-val 0.1) (cons "ocean" "water1.png")]
     [(< noise-val 0.2) (cons "beach" "beachsand1.png")]
@@ -44,39 +42,15 @@ ________________________________________________________________________________
 
 ;Creates a biome based on heeight
 (define (create-biome  height-value)
-  (let ([biome-data (get-noise-png height-value)])  
-  (hash
-   'name (car biome-data)
-   'pngPath (cdr biome-data))))
+  (let ([biome-data (get-biome-data height-value)])  
+    (hash
+     'name (car biome-data)
+     'pngPath (cdr biome-data))))
 
 (define (get-biome-png tile)(hash-ref (hash-ref tile 'biome) 'pngPath))
 
  
-
-#|
-________________________________"Interface" for noise_______________________________________________
-
-This is to allow different implemetnations of noise. All of the implementations require an x and a y.
-Any other parameters are up to the person implementing it
-
-Implementations look like this
-
-(define (function-name param-1 param-2...param-n)
-  (noise (lambda (x y) ...))
-____________________________________________________________________________________________________
-|#
-
-
-(struct noise (function))
-
-(define (noise-at noise x y)
-  ((noise-function noise) x y))
-
-
-
-
-#|
-________________________Noise Composition _________________________
+#|________________________Noise Composition _________________________
 The section below contains the functions that use noise composition
 To generate a final noise output value
 
@@ -89,8 +63,8 @@ amplitude_N * noise(octave_N * nx, octave_N * ny)
 Then we normalize the noise at the end using the sum of the amplitudes
 Represents the data we need for noise composition.
 Stores a list of amplitudes, octaves, and the total amplitud
-_______________________________________________________________
-|#
+_______________________________________________________________|#
+
 
 
 ;Contains the parameters we use to compose noise
@@ -98,13 +72,15 @@ _______________________________________________________________
 ;Where we iterate over the list of amplitude and octaves
 (struct noise-composition-params (amplitudes octaves total-amplitude ))
 
-;Used to create the noise-composition-params type struct
-(define (create-noise-composition-params amplitudes octaves)
-  (noise-composition-params amplitudes octaves (apply + amplitudes))) 
 
 ;Creates the noise-composition-params, which is based on
 ;the number of octaves. 
 (define (build-noise-composition-params num-octaves)
+
+  ;Used to create the noise-composition-params type struct
+  (define (create-noise-composition-params amplitudes octaves)
+    (noise-composition-params amplitudes octaves (apply + amplitudes))) 
+  
   (let ([amps '()]
         [octs '()])
     (for/list ([i (in-range 1 (+ num-octaves 1))])
@@ -115,39 +91,37 @@ _______________________________________________________________
     (create-noise-composition-params amps octs)))
 
 
-#|
-Composes the noise by adding several noise sources together using octaves and amplitudes.
-Adds an offset through a seed so we don't get the same valeu every time.
+;Composes the noise by adding several noise sources together using octaves and amplitudes.
+;Adds an offset through a seed so we don't get the same valeu every time.
+;Generate-octave-noise gets ONE sample of noiss, given an amplitdue, octave, and coordinates
+;noise-values calls generate-octave noise for all of the octaves and amplitudes
+;Then we normalize it at the end 
 
-Generate-octave-noise gets ONE sample of noiss, given an amplitdue, octave, and coordinates
-
-noise-values calls generate-octave noise for all of the octaves and amplitudes
-
-Then we normalize it at the end 
-|#
 (define (compose-noise source)
-
   (noise (lambda (x y)
-
-  (define (generate-octave-noise amplitude octave x y) ;Divides by the map width and height so we get values between 0-1
-    (* amplitude (abs(perlin (* octave (/ x MAP-WIDTH))
-                             (* octave (/ y MAP-HEIGHT))))))
-  (define noise-values
-    (for/list ([amp (noise-composition-params-amplitudes source)]
-               [oct (noise-composition-params-octaves source)])
-      (generate-octave-noise amp oct (+ x (get-seed)) (+ y (get-seed)))))
+           (define (generate-octave-noise amplitude octave x y) ;Divides by the map width and height so we get values between 0-1
+             (* amplitude (abs(perlin (* octave (/ x MAP-WIDTH))
+                                      (* octave (/ y MAP-HEIGHT))))))
+           (define noise-values
+             (for/list ([amp (noise-composition-params-amplitudes source)]
+                        [oct (noise-composition-params-octaves source)])
+               (generate-octave-noise amp oct (+ x (get-seed)) (+ y (get-seed)))))
            
-  (define normalized-noise (normalize-composition-noise (foldl + 0 noise-values) (noise-composition-params-total-amplitude source)))
-normalized-noise)))
+           (define normalized-noise (normalize-composition-noise (foldl + 0 noise-values) (noise-composition-params-total-amplitude source)))
+           normalized-noise)))
 
 ;normalizes the noise so that we have values between 0 and 1 using the sum of the amplitudes
 (define (normalize-composition-noise noise-value total-amplitude)
   (/ noise-value total-amplitude))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;                                Trigonometric Functions for Noise                                ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+#|______________Trigonometric Functions for Noise_________________________
+
+
+Applies modulation to a noise value
+_________________________________________________________________________|#                             
+
 
 ;Basic implementation of numpy linspace
 (define (linspace start stop num)
@@ -160,59 +134,79 @@ normalized-noise)))
 
 (define (linear-modulated-noise min max slope)
   (noise
-    (lambda (x y)
-      (define (linear-noise n slope) (* slope n)) ; Our linear function
+   (lambda (x y)
+     (define (linear-noise n slope) (* slope n)) ; Our linear function
 
-      (let ([linear-x (linear-noise x slope)]
-            [linear-y (linear-noise y slope)])
-        (abs (simplex (+ (+ linear-x) (get-seed)) (+ (+ y linear-y) (get-seed))))))))
+     (let ([linear-x (linear-noise x slope)]
+           [linear-y (linear-noise y slope)])
+       (abs (simplex (+ (+ linear-x) (get-seed)) (+ (+ y linear-y) (get-seed))))))))
 
 ;Gets an x and y sample from a sinusoid to buiild nose
 (define (sine-modulated-noise start-deg end-deg min max)
   (noise
-    (lambda (x y)
-      (define (sine-modulation start-deg end-deg min max sample-num)
-        (let ([radians (* (- end-deg start-deg) (/ pi 180))]
-              [dev (/ (- max min) 2)]
-              [base (/ (+ max min) 2)])
-          (+ (* dev (sin (* radians sample-num))) base min max)))
+   (lambda (x y)
+     (define (sine-modulation start-deg end-deg min max sample-num)
+       (let ([radians (* (- end-deg start-deg) (/ pi 180))]
+             [dev (/ (- max min) 2)]
+             [base (/ (+ max min) 2)])
+         (+ (* dev (sin (* radians sample-num))) base min max)))
       
-      (let ([sine-x (sine-modulation start-deg end-deg min max (list-ref width-sample-space x))]
-            [sine-y (sine-modulation start-deg end-deg min max y)])
-        (abs (perlin (+ (+ x sine-x) (get-seed) (+ (+ y sine-y) (get-seed)))))))))
+     (let ([sine-x (sine-modulation start-deg end-deg min max (list-ref width-sample-space x))]
+           [sine-y (sine-modulation start-deg end-deg min max y)])
+       (abs (perlin (+ (+ x sine-x) (get-seed) (+ (+ y sine-y) (get-seed)))))))))
 
 
-;(plot (points (map vector samples data) #:color 'red))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;                                Drawing and Tile Related                                                   ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+#|________________________________"Interface" for noise_______________________________________________
 
+This is to allow different implemetnations of noise. All of the implementations require an x and a y.
+Any other parameters are up to the person implementing it
+
+Implementations look like this
+
+(define (function-name param-1 param-2...param-n)
+  (noise (lambda (x y) ...))
+____________________________________________________________________________________________________|#
+
+
+
+(struct noise (function))
+
+(define (noise-at noise x y)
+  ((noise-function noise) x y))
+
+(define noise-composition (compose-noise (build-noise-composition-params 16)))
+(define linear-stuff (linear-modulated-noise 1 1 2))
+(define sine-noise (sine-modulated-noise 0 360 1 100))
+
+
+#|_______________________________Drawing and Tile Related___________________________________________
+
+Handles all the drawing operations
+____________________________________________________________________________________________________|#
 
 
 ;Travereses the map and chooses which tile to draw
 ;Selects a tile based off a noise value.
 ;Doesn't send the tile we draw to the canvas, that's handled by draw-tile-png
 (define (draw-tile-map canvas world-map)
+  (define (draw-tile-png dc x y image-path) ;Sends the drawing command to the canvas
+    (define image (read-bitmap image-path))
+    (send dc draw-bitmap image x y))
+
   (define dc (send canvas get-dc))
-  (for ([x (in-range MAP-WIDTH)])
-    (for ([y (in-range MAP-HEIGHT)])
-      (let ([tile (get-tile world-map x y)])
-                (draw-tile-png dc (hash-ref tile 'tile-width)
-                       (hash-ref tile 'tile-height ) (get-biome-png tile))))))
+  (for* ([x (in-range MAP-WIDTH)]
+         [y (in-range MAP-HEIGHT)])
+    (define tile (get-tile world-map x y))
+    (draw-tile-png dc
+                   (hash-ref tile 'tile-width)
+                   (hash-ref tile 'tile-height)
+                   (get-biome-png tile))))
 
+#|_______________________________Distance Metric Functions___________________________________________
 
+This section contains all of the distance function calculation variations
+____________________________________________________________________________________________________|#
 
-
-;Sends the drawing command to the canvas
-(define (draw-tile-png dc x y image-path)
-  (define image (read-bitmap image-path))
-  (send dc draw-bitmap image x y))
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;                                Basic Tile Related                                               ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;Returns the hash table at the coordinates
 (define (get-tile grid x y)
@@ -222,70 +216,68 @@ normalized-noise)))
 (define (square-bump x y)
   (- 1.0(* (- 1 (expt x 2)) (- 1 (expt y 2)))))
 
+(define (euclidian-squared x y)
+  (min 1 ( / (+ (expt x 2) (expt y 2)) (sqrt 2))))
+
+(define (distance-squared x y)
+  (- 1 (+ (expt x 2) (expt y 2))))
+
+(define (no-distance-function x y)
+  1)
+
+(define (lol-function x y)
+  3)
 
 
 ;Applies the distance function
+;Checks to see if the function passed as a parameter is no-distance-function. Then it returns the original noise value
+;I don't really like doing a conditional that checks for a specific procedure name, but that works for now. 
 (define (apply-reshape func x y noise-val)
-  (let ([distance (func x y)])
-    (/ (+ noise-val (- 1.0 distance)) 2)))
+  (if (eq? func no-distance-function) noise-val
+      (let ([distance (func x y)])
+        (/ (+ noise-val (- 1.0 distance)) 2))))
+
+  
+#|_______________________________World Map Related___________________________________________
+
+
+
+World map creation
+____________________________________________________________________________________________________|#
+
+
+
 
 
 ; Creates a tile with a noise value. The noise-func is any kind of noise generation function
 ; That has at least an x-cord and a y-cord. All of the arguments before x-cord and y-cord are curried
 ;nx and ny are used as inputs to the distance function we're using
-;At the moment using square bump distance
-(define (create-noise-tile x-cord y-cord noise)
+
+;The expt calculation for the temp noise. We use Math.pow(e * fudge_factor, exponent),
+(define (create-noise-tile x-cord y-cord noise distance-function redist-fudge redist-exp)
   (define nx (- (/ (* 2 x-cord) MAP-WIDTH) 1))
   (define ny (- (/ (* 2 y-cord) MAP-HEIGHT) 1))
-  (define temp-noise (noise-at noise x-cord y-cord))
-  (define final-noise (apply-reshape square-bump nx ny temp-noise))
+  (define temp-noise (expt (* (noise-at noise x-cord y-cord) redist-fudge) redist-exp  ))
+  (define final-noise (apply-reshape distance-function nx ny temp-noise))
   (hash 'x x-cord
         'y y-cord
         'tile-width (* x-cord TILE-SIZE)
         'tile-height (* y-cord TILE-SIZE)
-        'biome (create-biome temp-noise)))
-
-
-
-  
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;                                World Map Related                                                ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-(define noise-composition (compose-noise (build-noise-composition-params 3)))
-(define linear-stuff (linear-modulated-noise 1 1 2))
-(define sine-noise (sine-modulated-noise 0 360 1 100))
+        'biome (create-biome final-noise)))
 
 
 ;Creates the world map using a noise function
-(define (initialize-world-map)
+(define (initialize-world-map distance-function noise-function redist-fudge redist-exp)
   (define world-map (make-vector MAP-WIDTH))
   (for ([x-cord (in-range MAP-WIDTH)])
     (define row (make-vector MAP-HEIGHT))
     (for ([y-cord (in-range MAP-HEIGHT)])
 
-         (define temp-tile (create-noise-tile x-cord y-cord noise-composition))
-        (vector-set! row y-cord temp-tile))
+      (define temp-tile (create-noise-tile x-cord y-cord noise-function distance-function redist-fudge redist-exp))
+      (vector-set! row y-cord temp-tile))
 
     (vector-set! world-map x-cord row))
   world-map)
-
-
-;; Example usage:
-(define-world-map-generator
-  create-my-world
-  [(with-noise-function noise-composition)
-   (with-map-size 100 100)
-   (with-seed 42)
-   (with-distance-func square-bump)]
-  [(add-biome 0.1 'ocean "water1.png")
-   (add-biome 0.2 'beach "beachsand1.png")
-   (add-biome 0.3 'forest "tree1.png")
-   (add-biome 0.5 'jungle "jungle1.png")
-   (add-biome 0.7 'taiga "shrubland1.png")
-   (add-biome 0.9 'desert "deserttile1.png")
-   (add-biome 1.0 'snowlands "snowground1.png")])
 
 
 (provide (all-defined-out))
